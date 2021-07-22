eae800715c168b76e962fe87a4ad35217076dd0b
mong_head <camara1melon315@gmail.com>
2021-07-22 12:37:09 +0900

    feat(basic:webpack) : ex05(sass-loader)

---
+++ basic/webpack-practices/ex05/src/App.js
@@ -0,0 +1,11 @@
+import './assets/scss/App.scss';
+
+// DOM API
+const App = () => {
+    const app = document.createElement('h1'); // h1 node
+    app.classname = "Header";           // h1 node class
+    app.textContent = 'Hello world4'; // text node
+    return app;
+}
+
+export {App}
---
+++ basic/webpack-practices/ex05/src/index.js
@@ -0,0 +1,5 @@
+import { App } from './App'
+
+document
+    .getElementById('root')
+    .appendChild(App());
