ecf8518ad57d54e297e3caf592ae7a6b9c9bbc9f
mong_head <camara1melon315@gmail.com>
2021-07-22 14:32:44 +0900

    learn(basic:webpack) : image loader

---
+++ basic/webpack-practices/ex06/src/App.js
@@ -0,0 +1,17 @@
+import './assets/css/App.scss';
+import logo from './assets/images/logo.svg'
+
+// DOM API
+const App = () => {
+    const app = document.createElement('h1'); // h1 node
+    app.className = "App";           // h1 node class
+    app.innerHTML = 
+    `<header class='App-header'>
+       <img src='${logo }' class='App-logo' alt='logo' />
+       <p>Hello World</p>
+    </header>`;
+
+    return app;
+}
+
+export {App}
