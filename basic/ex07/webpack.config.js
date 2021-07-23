d41c2f87b858472a744d77b9ebd7ac5cb6dec271
mong_head <camara1melon315@gmail.com>
2021-07-23 15:25:21 +0900

    add(basic) : image 확장자 정규표현식

--- basic/ex07/webpack.config.js
+++ basic/ex07/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
             test: /\.css$/i,
             use: ['style-loader','css-loader']
         },{
-            test: /\.svg$/i,
+            test: /\.(svg|jpe?g|gif|png|tiff?|bmp|ico|)$/i,
             loader: 'file-loader',
             options: {
                 outputPath: '/assets/images',
