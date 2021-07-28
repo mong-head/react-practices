1c79ac138561fc1b7b5e39131a4a1371560bed91
mong_head <camara1melon315@gmail.com>
2021-07-29 08:55:52 +0900

    learn(component:guestbook): 줄바꿈

--- component/guestbook/src/Item.js
+++ component/guestbook/src/Item.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 
 export default function Item({name,message}){
     return(
         <li className="Guestbook__List__Item">
             <strong>{name}</strong>
             <p>
-                {message.replaceAll('\n',"<br/>")}
+                {message && message.split('\n').map((line,index)=> index > 0 ? 
+                    <Fragment>
+                        <br/>
+                        {line}
+                    </Fragment>: line)}
             </p>
             <strong></strong>
             <a href=''>삭제</a>
