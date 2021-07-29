0469ddfc68779eb8d60c9d660d8a51d929ed4b89
mong_head <camara1melon315@gmail.com>
2021-07-29 14:19:43 +0900

    modify(component:kanban): class css적용되도록 수정

--- component/kanban/src/ex02/Task.js
+++ component/kanban/src/ex02/Task.js
@@ -7,7 +7,7 @@ export default function Task({name,done}){
         <li className={styles.TaskList__Task}>
             <input type='checkbox' checked={done} />
             {name}
-            <a href='#' class='TaskList__Task--remove'></a>
+            <a href='#' className={styles['TaskList__Task--remove']}></a>
         </li>
     );
 };
